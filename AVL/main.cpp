40c67a568a2b15bdd12f183df6c6bc04666ca752
Eric Riddoch <eric.riddoch@gmail.com>
2017-11-16 13:41:53 -0700

    Initial Commit

---
+++ AVL/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AVL
+//
+//  Created by Eric Riddoch on 11/16/17.
+//  Copyright © 2017 Eric Riddoch. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
